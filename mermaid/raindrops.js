6c0296f628ef1615aaa605741f65268817a3e5a0
redsummernight <redsummernight@users.noreply.github.com>
2016-02-28 02:30:15 -0500

    Add purple water

--- mermaid/raindrops.js
+++ mermaid/raindrops.js
@@ -1,126 +1,131 @@
 /*
- *  Query UI plugin for raindrops on water effect.
- *  https://github.com/d-harel/raindrops.git
- */
-$.widget("water.raindrops", {
-    options: {
-        waveLength: 340,    // Wave Length. A numeric value. The higher the number, the smaller the wave length.
-        canvasWidth: 0,     // Width of the  water. Default is 100% of the parent's width
-        canvasHeight: 0,    // Height of the water. Default is 50% of the parent's height 
-        color: '#00aeef',   // Water Color
-        frequency: 3,       // Raindrops frequency. Higher number means more frequent raindrops.
-        waveHeight: 100 ,   // Wave height. Higher number means higher waves created by raindrops.
-        density: 0.02,      // Water density. Higher number means shorter ripples.
-        rippleSpeed:  0.1,  // Speed of the ripple effect. Higher number means faster ripples.
-        rightPadding: 20,    // To cover unwanted gaps created by the animation.
-        position:'absolute',
-        positionBottom:0,
-        positionLeft:0
-    },
-    _create: function () {
-        var canvas = window.document.createElement('canvas');
-        if (!this.options.canvasHeight) {
-            this.options.canvasHeight = this.element.height() / 2;
-        }
-        if (!this.options.canvasWidth) {
-            this.options.canvasWidth = this.element.width();
-        }
-        this.options.realWidth = this.options.canvasWidth + this.options.rightPadding;
-        canvas.height = this.options.canvasHeight;
-        canvas.width = this.options.realWidth;
+== Raindrops.js
+Raindrops effect plugin for jQuery UI
+http://daniellaharel.com/raindrops/
+https://github.com/d-harel/raindrops
 
-        this.ctx = canvas.getContext('2d');
-        this.ctx.fillStyle = this.options.color;
-        this.element.append(canvas);
+Modified version:
+- Remove dependencies on jQuery/jQuery UI
+- Resize canvas to always fill viewport's width
+*/
+var Raindrops = function(o) {
+    var self = this;
+    self._options = {};
+    self._options.color = o.color || '#00aeef';
+    self._options.canvasHeight = o.canvasHeight || 100;
+    self._options.waveLength = o.waveLength || 340;
+    self._options.waveHeight = o.waveHeight || 100;
+    // Raindrops frequency. Higher number means more frequent raindrops.
+    self._options.frequency = o.frequency || 3;
+    // Water density. Higher number means shorter ripples.
+    self._options.density = o.density || 0.02;
+    // Speed of the ripple effect. Higher number means faster ripples.
+    self._options.rippleSpeed = o.rippleSpeed || 0.1;
+
+    self._element = o.element;
+    self._create();
+};
+
+Raindrops.prototype = {
+    _getViewportWidth: function() {
+        var w = window, d = document, e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth;
+        return x;
+    },
+
+    _create: function() {
+        var self = this, o = self._options;
+        var canvas = window.document.createElement('canvas');
+        canvas.height = o.canvasHeight;
+        canvas.width = o.realWidth = self._getViewportWidth();
+
+        self.ctx = canvas.getContext('2d');
+        self._element.appendChild(canvas);
+
         canvas.parentElement.style.overflow = 'hidden';
         canvas.parentElement.style.position = 'relative';
-        canvas.style.position = this.options.position;
-        canvas.style.bottom = this.options.positionBottom;
-        canvas.style.left = this.options.positionLeft;
-        
-        this.springs = [];
-        for (var i = 0; i < this.options.waveLength; i++)
-        {
-            this.springs[i] = new this.Spring();
+        canvas.style.position = 'absolute';
+        canvas.style.bottom = 0;
+        canvas.style.left = 0;
+
+        self.springs = [];
+        for (var i = 0; i < o.waveLength; i++) {
+            self.springs[i] = new self.Spring();
         }
 
-        raindropsAnimationTick(this);
+        self._raindropsAnimationTick();
     },
-    Spring: function ()
-    {
+
+    Spring: function () {
         this.p = 0;
         this.v = 0;
-        //this.update = function (damp, tens)
-        this.update = function (density, rippleSpeed)
-        {
-            //this.v += (-tens * this.p - damp * this.v);
+        this.update = function (density, rippleSpeed) {
             this.v += (-rippleSpeed * this.p - density * this.v);
             this.p += this.v;
         };
     },
-    updateSprings: function (spread) {
+
+    _updateSprings: function(spread) {
+        var self = this, o = self._options;
         var i;
-        for (i = 0; i < this.options.waveLength; i++)
-        {
-            //this.springs[i].update(0.02, 0.1);
-            this.springs[i].update(this.options.density, this.options.rippleSpeed);
+        var springs = self.springs;
+        for (i = 0; i < o.waveLength; i++) {
+            springs[i].update(o.density, o.rippleSpeed);
         }
 
-        var leftDeltas = [],
-                rightDeltas = [];
-
+        var leftDeltas = [], rightDeltas = [];
         for (var t = 0; t < 8; t++) {
-
-            for (i = 0; i < this.options.waveLength; i++)
-            {
-                if (i > 0)
-                {
-                    leftDeltas[i] = spread * (this.springs[i].p - this.springs[i - 1].p);
-                    this.springs[i - 1].v += leftDeltas[i];
+            for (i = 0; i < o.waveLength; i++) {
+                if (i > 0) {
+                    leftDeltas[i] = spread * (springs[i].p - springs[i - 1].p);
+                    springs[i - 1].v += leftDeltas[i];
                 }
-                if (i < this.options.waveLength - 1)
-                {
-                    rightDeltas[i] = spread * (this.springs[i].p - this.springs[i + 1].p);
-                    this.springs[i + 1].v += rightDeltas[i];
+                if (i < o.waveLength - 1) {
+                    rightDeltas[i] = spread * (springs[i].p - springs[i + 1].p);
+                    springs[i + 1].v += rightDeltas[i];
                 }
             }
 
-            for (i = 0; i < this.options.waveLength; i++)
-            {
+            for (i = 0; i < o.waveLength; i++) {
                 if (i > 0)
-                    this.springs[i - 1].p += leftDeltas[i];
-                if (i < this.options.waveLength - 1)
-                    this.springs[i + 1].p += rightDeltas[i];
+                    springs[i - 1].p += leftDeltas[i];
+                if (i < o.waveLength - 1)
+                    springs[i + 1].p += rightDeltas[i];
             }
-
         }
-
     },
-    renderWaves: function ()
-    {
+
+    _renderWaves: function (){
+        var self = this, ctx = self.ctx, o = self._options;
         var i;
-        this.ctx.beginPath();
-        this.ctx.moveTo(0, this.options.canvasHeight);
-        for (i = 0; i < this.options.waveLength; i++)
-        {
-            this.ctx.lineTo(i * (this.options.realWidth / this.options.waveLength), (this.options.canvasHeight / 2) + this.springs[i].p);
+        ctx.beginPath();
+        ctx.moveTo(0, o.canvasHeight);
+        for (i = 0; i < o.waveLength; i++) {
+            ctx.lineTo(i * (o.realWidth / o.waveLength), (o.canvasHeight / 2) + self.springs[i].p);
         }
-        this.ctx.lineTo(this.options.realWidth, this.options.canvasHeight);
-        this.ctx.fill();
-    } 
-    
-});
+        ctx.lineTo(o.realWidth, o.canvasHeight);
+        ctx.fill();
+    },
 
-function raindropsAnimationTick(drop) {
-    if ((Math.random() * 100) < drop.options.frequency)
-        drop.springs[Math.floor(Math.random() * drop.options.waveLength)].p = drop.options.waveHeight;
+    _raindropsAnimationTick: function() {
+        var self = this, ctx = self.ctx, o = self._options;
+        if ((Math.random() * 100) < o.frequency) {
+            self.springs[Math.floor(Math.random() * o.waveLength)].p = o.waveHeight;
+        }
 
-    drop.ctx.clearRect(0, 0, drop.options.realWidth, drop.options.canvasHeight);
-    drop.updateSprings(0.1);
-    drop.renderWaves();
+        var w = self._getViewportWidth();
+        if (ctx.canvas.width < w) {
+            // 100% viewport width
+            ctx.canvas.width = o.realWidth = w;
+        }
+        ctx.fillStyle = o.color;
+        ctx.clearRect(0, 0, o.realWidth, o.canvasHeight);
+        self._updateSprings(0.1);
+        self._renderWaves();
 
-    requestAnimationFrame(function () {
-        raindropsAnimationTick(drop);
-    });
-}
-;
+        requestAnimationFrame(function () {
+            self._raindropsAnimationTick();
+        });
+    },
+};
