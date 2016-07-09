42123b4fcece6bb15e03c1cf44121da2175b4e8f
redsummernight <redsummernight@users.noreply.github.com>
2016-07-09 01:11:10 -0400

    Check external links weekly
    If an external link is valid, ignore it for a week before rechecking.

--- script/htmlproof.rb
+++ script/htmlproof.rb
@@ -2,11 +2,14 @@
 
 require 'html/proofer'
 
-HTML::Proofer.new("./_site", {
+HTML::Proofer.new('./_site', {
 	:parallel => {
 		:in_processes => 4,
 	},
 	:check_html => true,
+	:cache => {
+		:timeframe => '1w',
+	},
 	:typhoeus => {
 		:timeout => 15, # seconds
 	},
