2927eff11048a3aa9ad3122fb3ad91fabb81e02d
redsummernight <redsummernight@users.noreply.github.com>
2018-08-19 22:08:57 -0400

    Update gems, update link checker
    Disable SSL verification.
    Ignore failing site openclipart.org.
    Enable missing entity names check.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-gem 'github-pages', '= 167'
-gem 'jekyll', '= 3.6.2'
-gem 'html-proofer', '= 3.7.4'
+gem 'github-pages', '= 186'
+gem 'html-proofer', '= 3.9.2'
 gem 'wdm', '~> 0.1.1' if Gem.win_platform?
--- script/htmlproof.rb
+++ script/htmlproof.rb
@@ -3,19 +3,24 @@
 require 'html-proofer'
 
 HTMLProofer.check_directory('./_site', {
-	:parallel => {
-		:in_processes => 4,
-	},
 	:check_html => true,
 	:cache => {
 		:timeframe => '1w',
 	},
+	:parallel => {
+		:in_processes => 4,
+	},
 	:typhoeus => {
 		:ssl_verifypeer => false,
+		:ssl_verifyhost => 0,
 		:timeout => 15, # seconds
 	},
 	:url_ignore => [
 		/martin.ankerl.com/,
+		/openclipart.org/,
 		/soundcloud.com/,
 	],
+	:validation => {
+		:report_missing_names => true,
+	},
 }).run
