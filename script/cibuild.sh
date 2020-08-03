6ecf5477670af60f460634ad558c21df5f9e5725
redsummernight <redsummernight@users.noreply.github.com>
2020-08-02 21:33:15 -0400

    Use html5validator to check HTML and CSS files
    Fix HTML/CSS validation errors:
    
    - Update radial-gradient syntax.
    - Allow scrollbar-width.
    - Allow autocomplete on buttons to work around FF persisting the disabled state
      across page loads.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -5,4 +5,16 @@ set -e
 
 bundle exec jekyll build
 bundle exec ruby script/htmlproof.rb
-jshint --verbose --extract=auto --extra-ext=html,js ./_site/
+
+jshint --verbose --extract=auto --extra-ext=html,js _site/
+
+ignore_re=(
+	# Workaround for FF, which persists the dynamic disabled state of buttons
+	# across page loads: http://stackoverflow.com/q/5985839/
+	"Attribute \"autocomplete\" not allowed on element \"button\" at this point."
+	# Allow scrollbar-width, which is still in working draft
+	# https://developer.mozilla.org/en-US/docs/Web/CSS/scrollbar-width
+	"CSS: \"scrollbar-width\": Property \"scrollbar-width\" doesn't exist."
+)
+
+html5validator --also-check-css --root _site/ --ignore-re "${ignore_re[@]}" --log INFO
